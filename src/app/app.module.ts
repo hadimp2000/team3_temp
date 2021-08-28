11fb29cb843eef494d9431ecd06e543b7dd1ba66
hadi <hmp123475@gmail.com>
2021-08-28 16:54:33 +0430

    create layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { AsideComponent } from './layouts/aside/aside.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
