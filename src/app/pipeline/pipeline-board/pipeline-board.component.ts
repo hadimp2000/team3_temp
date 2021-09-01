1f9c2efcbb90d46938bedd8eb1ea3d8b9d13e3d5
perriex <parna80as@gmail.com>
2021-09-01 16:17:35 +0430

    zoom in/out added

--- src/app/pipeline/pipeline-board/pipeline-board.component.ts
+++ src/app/pipeline/pipeline-board/pipeline-board.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-pipeline-board',
   templateUrl: './pipeline-board.component.html',
-  styleUrls: ['./pipeline-board.component.scss']
+  styleUrls: ['./pipeline-board.component.scss'],
 })
 export class PipelineBoardComponent implements OnInit {
+  @ViewChild('board') board: ElementRef | null = null;
+  private zoom = 1;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  ngAfterViewInit() {
+    if (this.board) this.board.nativeElement.style.zoom = `${this.zoom}`;
   }
-
+  public zoomInCLick() {
+    this.zoom += 0.1;
+    if (this.board) this.board.nativeElement.style.zoom = `${this.zoom}`;
+  }
+  public zoomOutCLick() {
+    this.zoom -= 0.1;
+    if (this.board) this.board.nativeElement.style.zoom = `${this.zoom}`;
+  }
 }
