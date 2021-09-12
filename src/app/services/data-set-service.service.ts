dd990e6b0ca6db7da4265496d3e5e0fa100fd7c2
hadi <hmp123475@gmail.com>
2021-09-12 19:29:27 +0430

    edit pipeline table

--- src/app/services/data-set-service.service.ts
+++ src/app/services/data-set-service.service.ts
@@ -8,6 +8,9 @@ import {SendRequestService} from "./send-request-service.service";
   providedIn: 'root'
 })
 export class DataSetServiceService implements OnInit {
+  dataSets:object[]=[];
+  pipeLines:object[]=[];
+
 
   constructor() {
   }
@@ -32,38 +35,83 @@ export class DataSetServiceService implements OnInit {
       true,
 
     )
-    let dataSets: object[]=[];
-
     for (const csv of csvFiles) {
-      dataSets.push({
-          position: dataSets.length + 1,
+      this.dataSets.push({
+          position: this.dataSets.length + 1,
           name: csv,
           symbol: '',
           deleteIcon: ''
         }
       )
     }
-    return dataSets;
+    return this.dataSets;
   }
 
   async getCsvDataSet(name:string):Promise<any[]>{
-    const details={
-      filename:name,
-      token:localStorage.getItem('token')
-    }
     const {content}=await SendRequestService.sendRequest(
-      `https://localhost:5001/dataset/csv/${name}?token=${details.token}`,
+      `https://localhost:5001/dataset/csv/${name}?token=${localStorage.getItem('token')}`,
       true
     )
     return content;
   }
   async deleteCsvDataSet(name:string){
-    await SendRequestService.sendRequest(
+    let response=await SendRequestService.sendRequest(
       `https://localhost:5001/dataset/csv/delete/${name}?token=${localStorage.getItem('token')}`,
       false
     )
 
   }
 
-}
+  async getAllPipelines():Promise<any[]>{
+    const {pipelines} = await SendRequestService.sendRequest(
+      `https://localhost:5001/users/${localStorage.getItem('username')}/pipelines`,
+      true,
+    )
+    for (const pipeline of pipelines) {
+      this.pipeLines.push({
+          position: this.pipeLines.length + 1,
+          name: pipeline,
+          deleteIcon: ''
+        }
+      )
+    }
+    return this.pipeLines;
+  }
+
+  async createPipeline(){
+    let details:any;
+    if(this.pipeLines.length==0){
+      details={
+        name:`pipeline 1`,
+        content:''
+      }
+    }else{
+      // @ts-ignore
+      let name=this.pipeLines[this.pipeLines.length-1].name;
+      let number= parseInt(name.substr(name.length-1)) ;
+      details={
+        name: `pipeline ${number+1}`,
+        content:''
+      }
+    }
+
+    await SendRequestService.sendRequest(
+      `https://localhost:5001/pipeline/create?token=${localStorage.getItem('token')}`,
+      true,
+      details
+    )
+    location.reload();
+  }
+
+  async deletePipeline(name:string):Promise<any>{
+    return await SendRequestService.sendRequest(
+      `https://localhost:5001/pipeline/delete/${name}?token=${localStorage.getItem('token')}`,
+      true
+    )
+  }
+
+
+  }
+
+
 
