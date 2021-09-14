a18033007801a3f41c24a75af6660982edc178a1
perriex <parna80as@gmail.com>
2021-09-14 12:01:48 +0430

    add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {PageComponent} from "./layouts/page/page.component";
+import { PageComponent } from './layouts/page/page.component';
 import { LandingComponent } from './landing/landing.component';
-import {SignUpComponent} from "./identify/sign-up/sign-up.component";
-import {LogInComponent} from "./identify/log-in/log-in.component";
-import {PipelineComponent} from "./pipeline/pipeline.component";
-import {DatasetComponent} from "./dataset/dataset.component";
-import {DataSetSampleTableComponent} from "./dataset/data-set-sample-table/data-set-sample-table.component";
-import {PipelinesListComponent} from "./pipelines-list/pipelines-list.component";
+import { SignUpComponent } from './identify/sign-up/sign-up.component';
+import { LogInComponent } from './identify/log-in/log-in.component';
+import { PipelineComponent } from './pipeline/pipeline.component';
+import { DatasetComponent } from './dataset/dataset.component';
+import { DataSetSampleTableComponent } from './dataset/data-set-sample-table/data-set-sample-table.component';
+import { PipelinesListComponent } from './pipelines-list/pipelines-list.component';
+import { AuthService } from './identify/gaurd/auth.service';
 
 export const routes: Routes = [
   { path: 'signUp', component: SignUpComponent },
-  { path: '', component: LandingComponent },
-  {path:'logIn',component:LogInComponent},
-  {path:'pipeline/:id',component:PipelineComponent},
+  { path: 'logIn', component: LogInComponent },
+  {
+    path: 'pipeline/:id',
+    component: PipelineComponent,
+    canActivate: [AuthService],
+  },
   {
     path: 'pipelines',
+    canActivate: [AuthService],
     children: [
-      {path:'pipelinesList',component:PipelinesListComponent},
-      {path:'dataSet',component:DatasetComponent},
-      {path:'dataSet/:name',component:DataSetSampleTableComponent}
-
+      { path: 'pipelinesList', component: PipelinesListComponent },
+      { path: 'dataSet', component: DatasetComponent },
+      { path: 'dataSet/:name', component: DataSetSampleTableComponent },
     ],
     component: PageComponent,
   },
-  { path: '**', redirectTo: '' },
+  { path: '', component: LandingComponent },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthService],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
